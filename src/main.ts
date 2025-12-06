465296c8be5ef14e6e444c3f53f10f66c72cd318
olafsozanski <olaf.sozanski@gmail.com>
2025-12-06 23:12:12 +0100

    cors

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,11 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
+  app.enableCors({
+    origin: '*', // TODO: remove in production
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // TODO: remove in production
+  });
+
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Application running on: http://localhost:${process.env.PORT ?? 3000}`);
   console.log(`Swagger docs available at: http://localhost:${process.env.PORT ?? 3000}/api/docs`);
