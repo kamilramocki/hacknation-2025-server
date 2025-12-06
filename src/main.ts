ad3f877e5da63cadf33b93927a98f2d237d423b1
olafsozanski <olaf.sozanski@gmail.com>
2025-12-06 23:50:20 +0100

    cors

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'x-dupa'],
     credentials: false,
     exposedHeaders: ['Content-Type', 'Authorization'],
   });
